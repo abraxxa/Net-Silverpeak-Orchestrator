36d446a003970dcecdb2136a2f7a2ce678819e71
Alexander Hartmaier <alexander.hartmaier@t-systems.at>
2021-02-10 15:57:39 +0100

    initial version

---
+++ lib/Net/Silverpeak/Orchestrator.pm
@@ -0,0 +1,121 @@
+package Net::Silverpeak::Orchestrator;
+
+# ABSTRACT: Silverpeak Orchestrator REST API client library
+
+use 5.024;
+use Moo;
+use feature 'signatures';
+use Types::Standard qw( Str );
+use Carp qw( croak );
+use HTTP::CookieJar;
+use List::Util qw( any );
+# use Data::Dumper::Concise;
+
+no warnings "experimental::signatures";
+
+=head1 SYNOPSIS
+
+    use strict;
+    use warnings;
+    use Net::Silverpeak::Orchestrator;
+
+    my $orchestrator = Net::Silverpeak::Orchestrator->new(
+        server      => 'https://orchestrator.example.com',
+        user        => 'username',
+        passwd      => '$password',
+        clientattrs => { timeout => 30 },
+    );
+
+    $orchestrator->login;
+
+=head1 DESCRIPTION
+
+This module is a client library for the Silverpeak Orchestrator REST API.
+Currently it is developed and tested against version 9.0.2.
+
+=cut
+
+has 'user' => (
+    isa => Str,
+    is  => 'rw',
+);
+has 'passwd' => (
+    isa => Str,
+    is  => 'rw',
+);
+
+with 'Role::REST::Client';
+
+sub _build_user_agent ($self) {
+    require HTTP::Thin;
+    return HTTP::Thin->new(
+        %{$self->clientattrs},
+        cookie_jar => HTTP::CookieJar->new
+    );
+}
+
+sub _error_handler ($self, $res) {
+    my $error_message = $res->data;
+
+    croak('error (' . $res->code . '): ' . $error_message);
+}
+
+=method login
+
+Logs into the Silverpeak Orchestrator.
+
+=cut
+
+sub login($self) {
+    my $res = $self->post('/gms/rest/authentication/login', {
+        user     => $self->user,
+        password => $self->passwd,
+    });
+
+    $self->_error_handler($res)
+        unless $res->code == 200;
+
+    return 1;
+}
+
+=method logout
+
+Logs out of the Silverpeak Orchestrator.
+
+=cut
+
+sub logout($self) {
+    my $res = $self->get('/gms/rest/authentication/logout');
+    $self->_error_handler($res)
+        unless $res->code == 200;
+
+    return 1;
+}
+
+=method list_templategroups
+
+Returns an arrayref of template groups.
+
+=cut
+
+sub list_templategroups($self) {
+    my $res = $self->get('/gms/rest/template/templateGroups');
+    $self->_error_handler($res)
+        unless $res->code == 200;
+    return $res->data;
+}
+
+=method get_templategroup
+
+Returns a template group by name.
+
+=cut
+
+sub get_templategroup($self, $name) {
+    my $res = $self->get('/gms/rest/template/templateGroups/' . $name);
+    $self->_error_handler($res)
+        unless $res->code == 200;
+    return $res->data;
+}
+
+1;
---
+++ t/api.t
@@ -0,0 +1,57 @@
+use Test2::V0;
+use Test2::Tools::Compare qw( array hash );
+use Net::Silverpeak::Orchestrator;
+# use JSON qw();
+
+SKIP: {
+    skip_all "environment variables not set"
+        unless (exists $ENV{NET_SILVERPEAK_ORCHESTRATOR_HOSTNAME}
+            && exists $ENV{NET_SILVERPEAK_ORCHESTRATOR_USERNAME}
+            && exists $ENV{NET_SILVERPEAK_ORCHESTRATOR_PASSWORD}
+            && exists $ENV{NET_SILVERPEAK_ORCHESTRATOR_POLICY});
+};
+
+my $orchestrator = Net::Silverpeak::Orchestrator->new(
+    server      => 'https://' . $ENV{NET_SILVERPEAK_ORCHESTRATOR_HOSTNAME},
+    user        => $ENV{NET_SILVERPEAK_ORCHESTRATOR_USERNAME},
+    passwd      => $ENV{NET_SILVERPEAK_ORCHESTRATOR_PASSWORD},
+    clientattrs => { timeout => 30 },
+);
+
+ok($orchestrator->login, 'login to Silverpeak Orchestrator successful');
+
+is($orchestrator->list_templategroups,
+    array {
+        etc();
+    },
+    'list_templategroups ok');
+
+like(
+    dies { $orchestrator->get_templategroup('not-existing') },
+    qr/Failed to get Templates for group/,
+    'get_templategroup for not existing template group throws exception'
+);
+
+is(my $templategroup = $orchestrator->get_templategroup('LocalSecurity'),
+    hash {
+        etc();
+    },
+    'get_templategroup for existing template group ok');
+
+use Data::Dumper::Concise;
+# $Data::Dumper::Maxdepth = 2;
+# print Dumper($templategroup->{selectedTemplates});
+
+my ($security_template) = grep { $_->{name} eq 'securityMaps' }
+    $templategroup->{selectedTemplates}->@*;
+
+ok($security_template, 'securityMaps template found in template group');
+
+ok(exists $security_template->{value}->{data}->{map1}->{'0_0'}->{prio}
+    && ref $security_template->{value}->{data}->{map1}->{'0_0'}->{prio}
+        eq 'HASH',
+    'securityMaps return data structure as expected');
+
+print Dumper($security_template->{value}->{data}->{map1}->{'0_0'}->{prio});
+
+done_testing();
